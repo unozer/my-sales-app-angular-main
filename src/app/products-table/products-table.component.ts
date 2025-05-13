7a4c2930d28b6728cb570ced6e7cbeaa33d526c2
unozer <stra.perrino@gmail.com>
2025-05-13 15:23:04 +0200

    products table style + add cart btn

--- src/app/products-table/products-table.component.ts
+++ src/app/products-table/products-table.component.ts
@@ -4,17 +4,32 @@ import { Product } from '../products/product.dto';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
 import { RouterModule } from '@angular/router';
+import { MatIcon } from '@angular/material/icon';
+import { CartItem } from '../cart.dto';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-products-table',
   standalone: true,
-  imports: [CommonModule, MatTableModule, RouterModule],
+  imports: [MatButtonModule, CommonModule, MatTableModule, RouterModule, MatIcon],
   templateUrl: './products-table.component.html',
   styles: ``
 })
 export class ProductsTableComponent {
+
   @Input() products: Product[] = [];
-  columnNames: string[] = ['name', 'unitPrice', 'unitsInStock', 'category', 'discontinued'];
-  
+  columnNames: string[] = ['name', 'unitPrice', 'unitsInStock', 'category', 'discontinued', 'addToCart'];
+
   constructor(private cartService: CartService) { }
+
+  onAddToCart(product: Product) {
+    const cartItem: CartItem = {
+      idProduct: product.id,
+      name: product.name,
+      quantity: 1,
+      unitPrice: product.unitPrice,
+    };
+    this.cartService.addItem(cartItem);
+  }
 }

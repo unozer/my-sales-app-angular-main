ffbdf8f6512c837d98dcd0cbe8aaf9e82014709d
unozer <stra.perrino@gmail.com>
2025-05-13 11:34:57 +0200

    add to cart

--- src/app/products/products-card/products-card.component.ts
+++ src/app/products/products-card/products-card.component.ts
@@ -2,6 +2,8 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { Product } from '../product.dto';
+import { CartItem } from '../../cart.dto';
+import { CartService } from '../../cart.service';
 
 @Component({
     selector: 'app-products-card',
@@ -10,9 +12,18 @@ import { Product } from '../product.dto';
     styles: ``
 })
 export class ProductsCardComponent {
+
+  constructor(private cartService: CartService) {}
+
   @Input() product: Product;
 
   onAddToCart(product: Product) {
-    console.log("TODO")
+    const cartItem: CartItem = {
+      idProduct: product.id,
+      name: product.name,
+      quantity: 1,
+      unitPrice: product.unitPrice,
+    };
+    this.cartService.addItem(cartItem);
   }
 }
